44e41b6ebf733d77ce96f9ae72f46b9daba72c8b
xiaogao66 <xiaogaola@qq.com>
2017-09-10 15:01:43 +0800

    add

---
+++ src/main/java/com/xg/enums/SiteEnum.java
@@ -0,0 +1,52 @@
+package com.xg.enums;
+
+/**
+ * Created by xg on 2017/9/9.
+ */
+public enum SiteEnum {
+    WX(1, "http://www.23wx.cc"),
+    XS(2, "http://www.xs.la");
+    private int id;
+    private String url;
+
+    SiteEnum(int id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public static SiteEnum getEnumById(int id) {
+        switch (id) {
+            case 1:
+                return WX;
+            case 2:
+                return XS;
+            default:
+                throw new RuntimeException("id=" + id + "不支持此站点");
+        }
+    }
+    public static SiteEnum getEnumByUrl(String url){
+        for (SiteEnum s:values()
+             ) {
+            if(url.contains(s.getUrl())){
+                return s;
+            }
+        }
+        throw  new RuntimeException("url="+url+"不支持此站点");
+    }
+}
