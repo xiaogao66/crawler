3f2fb6ca39bbf07f5c1a1941186e350aae207a3f
xiaogao66 <xiaogaola@qq.com>
2017-09-10 15:03:43 +0800

    add method testGetSiteRule

--- src/test/java/com/xg/impl/TestGetChapter.java
+++ src/test/java/com/xg/impl/TestGetChapter.java
@@ -1,8 +1,10 @@
 package com.xg.impl;
 
 import com.xg.entitys.Chapter;
+import com.xg.enums.SiteEnum;
 import com.xg.interfaces.IChapterCrawler;
 import com.xg.impl.ChapterCrawler;
+import com.xg.util.ChapterUtil;
 import org.junit.Test;
 
 import java.util.List;
@@ -14,10 +16,17 @@ public class TestGetChapter {
     @Test
     public void test() {
         IChapterCrawler crawler=new ChapterCrawler();
-        List<Chapter> chapters= crawler.getChapter("http://www.xs.la/0_5/");
+        //List<Chapter> chapters= crawler.getChapter("http://www.xs.la/0_5/");
+        List<Chapter> chapters= crawler.getChapter("http://www.23wx.cc/du/83/83293/");
         for (Chapter chapter:chapters) {
             System.out.println( chapter.toString());
         }
+    }
+
+    @Test
+    public void testGetSiteRule(){
+        ChapterUtil.getParseText(SiteEnum.getEnumByUrl("http://www.23wx.cc/du/83/83293/"));
+        System.out.println( ChapterUtil.getParseText(SiteEnum.getEnumByUrl("http://www.23wx.cc/du/83/83293/")));
     }
 }
 
