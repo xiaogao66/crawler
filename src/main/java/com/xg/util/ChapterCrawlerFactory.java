1b0341e2a35583ef567be486138183600381650e
xiaogao66 <xiaogaola@qq.com>
2017-09-13 23:41:20 +0800

    new file

---
+++ src/main/java/com/xg/util/ChapterCrawlerFactory.java
@@ -0,0 +1,33 @@
+package com.xg.util;
+
+import com.xg.enums.SiteEnum;
+import com.xg.impl.BXWXChapterCrawler;
+import com.xg.impl.ChapterCrawler;
+import com.xg.interfaces.IChapterCrawler;
+
+/**
+ * Created by xg on 2017/9/13.
+ */
+public final class ChapterCrawlerFactory {
+    /*
+    *returns the implementation class that implements the IChapterCrawler interface
+
+     */
+    private  ChapterCrawlerFactory(){};
+
+    public static IChapterCrawler getChapterCrawler(String url){
+        SiteEnum siteEnum=SiteEnum.getEnumByUrl(url);
+        IChapterCrawler chapterCrawler=null;
+        switch (siteEnum){
+            case BXWX:
+                chapterCrawler=new BXWXChapterCrawler();break;
+            case WX:
+            case XS:
+                chapterCrawler=new ChapterCrawler();
+        }
+       return chapterCrawler;
+    }
+
+
+
+}
---
+++ src/main/java/com/xg/util/ChapterDetailCrawlerFactory.java
@@ -0,0 +1,27 @@
+package com.xg.util;
+
+import com.xg.enums.SiteEnum;
+import com.xg.impl.ChapterDetailCrawler;
+import com.xg.interfaces.IChapterDetailCrawler;
+
+/**
+ * Created by xg on 2017/9/13.
+ */
+public final  class ChapterDetailCrawlerFactory {
+    /*
+    returns the implementation class that implements the IChapterCrawler interface
+     */
+    private ChapterDetailCrawlerFactory(){};
+    public static IChapterDetailCrawler getChapterDetailCrawler(String url){
+        SiteEnum siteEnum=SiteEnum.getEnumByUrl(url);
+        IChapterDetailCrawler chapterDetailCrawler=null;
+        switch (siteEnum){
+            case XS:
+            case WX:
+            case BXWX:
+                chapterDetailCrawler=new ChapterDetailCrawler();
+        }
+        return chapterDetailCrawler;
+    }
+
+}
